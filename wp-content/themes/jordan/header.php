c3d68297f89f6de4e2dd99cc913d8cced2d7eaa6
Jordan Barber <wjbarber3@gmail.com>
2017-01-25 15:05:48 -0700

    initial commit

---
+++ wp-content/themes/jordan/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?> >
+
+<head>
+    <title>Jordan Barber - <?php the_title(); ?></title>
+    <meta http-equiv="X-UA-compatible" content="IE=edge" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-touch-icon.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-touch-icon-precomposed.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class( $class ); ?> >
+
+<?php include_once("svg/svg-defs.svg"); ?>
+
+<!-- <header>
+    <div class="main-wrap">
+        <a class="logo" href="/"><svg width="326" height="69"><use xlink:href="#logo"></use></svg></a>
+        <i class="mobile-menu-trigger fa fa-bars"></i>
+    </div>
+</header> -->
---
+++ wp-content/themes/jordan/js/main.js
@@ -0,0 +1,18 @@
+(function($){
+
+    /*
+    |--------------------------------------------------------------------------
+    | Header Functionality
+    |--------------------------------------------------------------------------
+    | This object houses any functionality associated with the header
+    |
+    */
+    Header = {
+        
+    }
+
+    $(document).ready(function() {
+        // Header.init();
+    })
+
+})(jQuery);
